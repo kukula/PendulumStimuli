e1e5f26ba2028f8392b1592b96d242f06d6a4e24
Tolik <anatolkukula@gmail.com>
2023-11-15 23:28:44 -0500

    Initial Commit

---
+++ PendulumStimuli/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PendulumStimuli
+//
+//  Created by Tolik Kukul on 15/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PendulumStimuli/PendulumStimuliApp.swift
@@ -0,0 +1,17 @@
+//
+//  PendulumStimuliApp.swift
+//  PendulumStimuli
+//
+//  Created by Tolik Kukul on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct PendulumStimuliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
